d8019b33bcd32b32ac74a4bc387a96d298d5457a
TedMarozzi <marozzi.t@gmail.com>
2020-05-21 12:50:23 +1000

    uploaded Project 2A design pdf and added Tower and air support calsses

---
+++ src/AirSupport.java
@@ -0,0 +1,9 @@
+public class AirTower extends Tower {
+
+    private static final String imgPath = "res/images/airsupport.png";
+    private static int price = 500;
+
+    public AirTower(String imgPath, int cost) {
+        super(imgPath, cost);
+    }
+}
